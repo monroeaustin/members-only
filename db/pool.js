b1f10e905e096d3059739a19b57f3a8c886a70ce
Monroe Austin <monroeraustin2@yahoo.com>
2025-08-10 14:59:41 -0700

    Fix: Prod DB Link

--- db/pool.js
+++ db/pool.js
@@ -7,10 +7,6 @@ const { Pool } = require("pg");
 
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT
+ connectionString: process.env.CONNECTION
 
 });
