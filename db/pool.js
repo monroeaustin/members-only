e5410b5fd50f5832613ca7841557d55be76ee96a
Monroe Austin <monroeraustin2@yahoo.com>
2025-08-10 15:34:02 -0700

    Fix: Railway Variable DB Connection

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 
 
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.CONNECTION,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+module.exports = new Pool({
+ connectionString:DATABASE_URL
+
 });
